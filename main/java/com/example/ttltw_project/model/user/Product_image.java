dfbfc4736b7e39cb46ef1ed52c3b7cf5b0f3d350
Kim Tuyến <23130377@st.hcmuaf.edu.vn>
2026-04-04 21:55:50 +0700

    up file product_img

---
+++ main/java/com/example/ttltw_project/model/user/Product_image.java
@@ -0,0 +1,35 @@
+package com.example.ttltw_project.model.user;
+
+public class Product_image {
+    private int id;
+    private int product_id;
+    private String image_url;
+
+    public Product_image() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+}
+
